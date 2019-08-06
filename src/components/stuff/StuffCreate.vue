c8fe41f2fa1ec06abe64d7dbff0478f640eb26df
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-08-07 00:43:49 +0300

    Task-#5 CRUD Stuff

--- src/components/stuff/StuffCreate.vue
+++ src/components/stuff/StuffCreate.vue
@@ -1,11 +1,35 @@
 <template>
 <div id="stuff-create">
-    stuff-create
+    <bread-crumbs></bread-crumbs>
+    <section class="stuff-create-wrapper">
+        <form id="create-form" class="form form-wrapper">
+            <label class="form__label" for="name">Name of stuff</label>
+            <input  class="form__input" id="name" v-model="name" name="name" type="text">
+            <div v-on:click="sendForm()" class="send-form">
+                <a href="/stuff" class="button__create" >Create</a>
+            </div>
+        </form>
+    </section>
 </div>
 </template>
 
 <script>
+import "../../assets/scss/imports/stuff_create.scss";
+import BreadCrumbs from "../mixins/BreadCrumbs.vue";
+import axios from 'axios';
 export default {
-
+    components: {
+        BreadCrumbs
+    },
+    data:() => {
+        return {
+            name
+        }
+    },
+    methods: {
+        sendForm: function() {
+            axios.post(`${process.env.VUE_APP_API_BASE_URL}stuff`,{name: this.name});
+        },
+    }
 }
 </script>
--- src/components/stuff/StuffUpdate.vue
+++ src/components/stuff/StuffUpdate.vue
@@ -1,11 +1,46 @@
 <template>
 <div id="stuff-update">
-    stuff-update
+    <bread-crumbs></bread-crumbs>
+    {{setStuffId($route.params.id)}}
+    <section class="stuff-create-wrapper">
+        <form id="create-form" class="form form-wrapper">
+            <label class="form__label" for="name">Name of stuff</label>
+            <input  class="form__input" v-model="name" id="name" name="name" type="text" :placeholder = "oldName" >
+            <div v-on:click="sendForm()" class="send-form">
+                <a href="/stuff" class="button__create" >Update</a>
+            </div>
+        </form>
+    </section>
+
 </div>
 </template>
 
 <script>
+import BreadCrumbs from "../mixins/BreadCrumbs.vue";
+import axios from 'axios';
 export default {
-
+    name: "StuffUpdate",
+    components: {
+        BreadCrumbs
+    },
+    data: () => {
+        return {
+            name,
+            stuffId: '',
+            oldName: ''
+        }
+    },
+    methods: {
+        setStuffId: function(id) {
+            this.stuffId = id;
+        },
+        sendForm: function() {
+            axios.put(`${process.env.VUE_APP_API_BASE_URL}stuff/${this.stuffId}`,{name: this.name});
+        }
+    },
+    mounted: async function() {
+        const spicesRequest = await axios.get(`${process.env.VUE_APP_API_BASE_URL}stuff/${this.spiceId}`);
+        this.oldName = await spicesRequest.data.name;
+    }
 }
 </script>
